e6ace81c4c91610a5ede151ea59146566f7c95bd
Artyom Bibikov <artembibikov75@gmail.com>
2025-12-22 00:58:40 +0300

    Try a different version

--- 05_hw/main.c
+++ 05_hw/main.c
@@ -1,39 +1,39 @@
 #include "stack.h"
-
 #include <stdio.h>
 
 int main() {
     Stack stack;
     initStack(&stack);
 
-    push(&stack, 30);
-    push(&stack, 100);
     push(&stack, 20);
+    push(&stack, 10);
+    push(&stack, 30);
 
     printf("After pushing elements:\n");
     traverseStack(&stack);
 
-	int popped_value;
+    int popped_value;
     if (pop(&stack, &popped_value)) {
         printf("Popped value: %d\n", popped_value);
     }
 
-    pop(&stack);
-
     printf("After popping an element:\n");
     traverseStack(&stack);
 
-    if (searchByValue(&stack, 20) != NULL) {
+    Node* searchResult = searchByValue(&stack, 20);
+    if (searchResult != NULL) {
         printf("Element with value 20 found.\n");
     }
     else {
         printf("Element with value 20 not found.\n");
     }
 
-    const Node* topElement = getTop(&stack);
+    Node* topElement = getTop(&stack);
     if (topElement != NULL) {
         printf("Top element: %d\n", topElement->data);
     }
+
+    printf("Peek top element: %d\n", top(&stack));
 
     while (!isEmpty(&stack)) {
         pop(&stack, &popped_value);
@@ -43,5 +43,3 @@ int main() {
     destroyStack(&stack);
     return 0;
 }
-
-
